9956d626eccc646bb1357803e34edcacfc196563
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-12-29 12:45:24 +0900

    [Django : Portfolio] Title : admin 사이트 버그 수정, createsuperuser 등록
    기본적인 createsuperuser가 돌아가게 설정 - 세부사항 설정 필요
    admin 사이트에 css,js,font,img 가 적용이 안되는 버그 수정

--- Web_Portfolio/config/urls.py
+++ Web_Portfolio/config/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('content/',include('polls.content.urls')),
     path('user/',include('polls.user.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
